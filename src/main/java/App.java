09b9d5c4166183ecacc489b1984ee2175c99d429
Artem Baykov <baikoff@bk.ru>
2022-04-21 19:39:14 +0300

    Добавление класса Cat

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,16 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        Cat bean1 = (Cat) applicationContext.getBean("cat");
+        System.out.println(bean1.getMessage());
+        Cat bean2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(bean2.getMessage());
+        HelloWorld bean3 = (HelloWorld) applicationContext.getBean("hellowold");
+        System.out.println(bean3.getMessage());
+
+        boolean srav = bean == bean3;
+        boolean srav1 = bean2 == bean1;
+        System.out.println(srav);
+        System.out.println(srav1);
     }
 }
